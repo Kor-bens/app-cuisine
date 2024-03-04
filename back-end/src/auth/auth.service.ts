10fe89f85cb5545fad889478efcf2969dc90b688
Tortosa <sofianetortosa@gmail.com>
2024-03-04 18:51:40 +0100

    suppression auth

--- back-end/src/auth/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/shared/prisma/prisma.service';
-
-@Injectable()
-export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
-
-    
-}
