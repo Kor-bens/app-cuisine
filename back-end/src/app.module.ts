f638a061e0fb5a5c8a8d92c2a6a1e87fd3d05f48
Tortosa <sofianetortosa@gmail.com>
2024-03-04 18:54:42 +0100

    modif nommage

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,18 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { RecetteModule } from './recette/recette.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule,
-    PrismaModule,
-    RecetteModule,
-    AuthModule
-  ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [ConfigModule, PrismaModule, RecetteModule, UserModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
