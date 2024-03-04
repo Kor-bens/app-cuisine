34c9f4e59dcd779e22cbbfc8a927f9cfcac03c7b
Tortosa <sofianetortosa@gmail.com>
2024-03-04 18:53:13 +0100

    post user

---
+++ back-end/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto)
+  }
+}
