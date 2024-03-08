67b577e114f5ea7545d9608be6d0a0623c0803f6
Tortosa <sofianetortosa@gmail.com>
2024-03-08 14:06:03 +0100

    style

--- front-end/src/components/Inscription/Inscription.jsx
+++ front-end/src/components/Inscription/Inscription.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./inscription.css"
+import NavBar from "../common/navbar/Navbar";
 
 function FormulaireInscription() {
   const [formData, setFormData] = useState({
@@ -69,14 +70,18 @@ function FormulaireInscription() {
 
   return (
     <>
-      <h1 className="px-8 text-center text-xl">Créer un compte</h1>
-    <form onSubmit={handleSubmit} className="flex flex-col w-5/5 mt-20 px-8">
+    <NavBar />
+    <form onSubmit={handleSubmit} className=" flex flex-col w-5/5 px-8 
+    md:text-center md:items-center md:justify-center md:h-screen md:w-full">
+      <h1 className="px-8 text-center md:text-5xl mb-6">Créer un compte</h1>
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       <div className="input-group ">
-        <label htmlFor="nom">Nom</label>
+        <label htmlFor="nom" className="md:text-3xl">Nom</label>
         <div className="input-container px-10">
           <input
-            className="bg-orange-100 rounded-md mobile:w-full"
+            className="bg-orange-100 rounded-md
+            mobile:w-full 
+            md:w-full md:mb-3 md:mt-3 md:text-2xl"
             type="text"
             name="nom"
             placeholder="nom"
@@ -86,10 +91,12 @@ function FormulaireInscription() {
         </div>
       </div>
       <div className="input-group">
-        <label htmlFor="prenom">Prénom</label>
+        <label htmlFor="prenom" className="md:text-3xl">Prénom</label>
         <div className="input-container">
           <input
-            className="bg-orange-100 rounded-md mobile:w-full"
+            className="bg-orange-100 rounded-md 
+            mobile:w-full 
+            md:w-full md:mb-3 md:mt-3 md:text-2xl"
             type="text"
             name="prenom"
             placeholder="prénom"
@@ -99,10 +106,12 @@ function FormulaireInscription() {
         </div>
       </div>
       <div className="input-group">
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email" className="md:text-3xl">Email</label>
         <div className="input-container">
           <input
-            className="bg-orange-100 rounded-md mobile:w-full"
+            className="bg-orange-100 rounded-md
+             mobile:w-full
+              md:w-full md:mb-3 md:mt-3 md:text-2xl"
             type="email"
             name="email"
             placeholder="email"
@@ -112,41 +121,49 @@ function FormulaireInscription() {
         </div>
       </div>
       <div className="input-group">
-        <label htmlFor="motDePasse">Mot de passe</label>
+        <label htmlFor="motDePasse" className="md:text-3xl">Mot de passe</label>
         <div className="input-container">
           <input
-            className="bg-orange-100 rounded-md mobile:w-full"
+            className="bg-orange-100 rounded-md
+             mobile:w-full 
+             md:w-full md:mb-3 md:mt-3 md:text-2xl"
             type={showPassword ? "text" : "password"}
             name="motDePasse"
             placeholder="mot de passe"
             value={formData.motDePasse}
             onChange={handleChange}
           />
-          <button type="button" onClick={toggleShowPassword}>
+          <button type="button" onClick={toggleShowPassword}
+          className="md:text-2xl">
             {showPassword ? "Cacher" : "Afficher"}
           </button>
         </div>
       </div>
       <div className="input-group">
-        <label htmlFor="confirmationMotDePasse">
+        <label htmlFor="confirmationMotDePasse" className="md:text-3xl">
           Confirmation du mot de passe
         </label>
         <div className="input-container">
           <input
-            className="bg-orange-100 rounded-md mobile:w-full"
+            className="bg-orange-100 rounded-md
+             mobile:w-full md:mb-3 md:mt-3 md:text-2xl"
             type={showPasswordConfirm ? "text" : "password"}
             name="confirmationMotDePasse"
             placeholder="confirmation de mot de passe"
             value={formData.confirmationMotDePasse}
             onChange={handleChange}
           />
-          <button type="button" onClick={toggleShowPasswordConfirm}>
+          <button type="button" onClick={toggleShowPasswordConfirm}
+          className="md:text-2xl">
             {showPasswordConfirm ? "Cacher" : "Afficher"}
           </button>
         </div>
       </div>
       <div>
-        <button className="bg-orange-100 rounded-md mt-5 mobile:w-full px-8" type="submit">
+        <button className="bg-orange-100 rounded-md mt-5 px-8 
+         mobile:w-full 
+         md:text-2xl" 
+         type="submit">
           S'inscrire
         </button>
       </div>
