628f323bccd757f886ae37a34d3c8facd66bbb93
Tortosa <sofianetortosa@gmail.com>
2024-03-09 02:51:57 +0100

    STYLE

--- front-end/src/components/Inscription/Inscription.jsx
+++ front-end/src/components/Inscription/Inscription.jsx
@@ -83,111 +83,110 @@ function FormulaireInscription() {
         onSubmit={handleSubmit}
         className=" flex flex-col w-5/5 px-8 
         mobile:mb-4 mobile:mt-4 mobile:w-5/5 mobile:justify-center mobile:h-full
-    md:text-center  md:justify-center md:h-full md:w-5/5 md:mt-4 md:mb-4">
+    md:text-center  md:justify-center md:h-full md:w-5/5 md:mt-16">
         <h1 className="px-8 text-center text-slate-200 mb-6 font-bold
         mobile:text-3xl
         md:text-5xl">Créer un compte</h1>
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        {errorMessage && <p className="text-red-500 md:text-3xl md:font-bold">{errorMessage}</p>}
         <div className="input-group ">
-          <label htmlFor="nom" className=" text-slate-200 font-bold
+          {/* <label htmlFor="nom" className=" text-slate-200 font-bold
           md:text-3xl ">
             Nom
-          </label>
+          </label> */}
           <div className="input-container md:px-48">
             <input
               className="bg-orange-100 rounded-md text-center
             mobile:w-full 
-            md:w-full md:mb-3 md:mt-3 md:text-2xl md:h-12"
+            md:w-full md:mb-4 md:mt-6 md:text-2xl md:h-12"
               type="text"
               name="nom"
-              placeholder="nom"
+              placeholder="Nom"
               value={formData.nom}
               onChange={handleChange}
             />
           </div>
         </div>
         <div className="input-group">
-          <label htmlFor="prenom" className="text-slate-200 font-bold 
+          {/* <label htmlFor="prenom" className="text-slate-200 font-bold 
           md:text-3xl">
             Prénom
-          </label>
+          </label> */}
           <div className="input-container md:px-48">
             <input
               className="bg-orange-100 rounded-md text-center 
             mobile:w-full 
-            md:w-full md:mb-3 md:mt-3 md:text-2xl md:h-12"
+            md:w-full md:mb-4 md:mt-4 md:text-2xl md:h-12"
               type="text"
               name="prenom"
-              placeholder="prénom"
+              placeholder="Prénom"
               value={formData.prenom}
               onChange={handleChange}
             />
           </div>
         </div>
         <div className="input-group">
-          <label htmlFor="email" className="text-slate-200 font-bold 
+          {/* <label htmlFor="email" className="text-slate-200 font-bold 
           md:text-3xl">
             Email
-          </label>
+          </label> */}
           <div className="input-container md:px-48">
             <input
               className="bg-orange-100 rounded-md text-center
              mobile:w-full
-              md:w-full md:mb-3 md:mt-3 md:text-2xl md:h-12"
+              md:w-full md:mb-4 md:mt-4 md:text-2xl md:h-12"
               type="email"
               name="email"
-              placeholder="email"
+              placeholder="Email"
               value={formData.email}
               onChange={handleChange}
             />
           </div>
         </div>
         <div className="input-group">
-          <label htmlFor="motDePasse" className="text-slate-200 font-bold 
+          {/* <label htmlFor="motDePasse" className="text-slate-200 font-bold 
           md:text-3xl">
             Mot de passe
-          </label>
-          <div className="input-container md:px-48">
-            <input
-              className="bg-orange-100 rounded-md text-center
-             mobile:w-full 
-             md:w-full md:mb-3 md:mt-3 md:text-2xl md:h-12"
-              type={showPassword ? "text" : "password"}
-              name="motDePasse"
-              placeholder="mot de passe"
-              value={formData.motDePasse}
-              onChange={handleChange}
-            />
-            <button
-              type="button"
-              onClick={toggleShowPassword}
-              className="text-slate-200 font-bold
-              md:text-2xl"
-            >
-              {showPassword ? "Cacher" : "Afficher"}
-            </button>
-          </div>
+          </label> */}
+          <div className="input-container md:px-48 relative">
+  <input
+    className="bg-orange-100 rounded-md text-center
+     mobile:w-full 
+     md:w-full md:mb-4 md:mt-4 md:text-2xl md:h-12 md:pl-3 md:pr-16"
+    type={showPassword ? "text" : "password"}
+    name="motDePasse"
+    placeholder="Mot de passe"
+    value={formData.motDePasse}
+    onChange={handleChange}
+  />
+  <button
+    type="button"
+    onClick={toggleShowPassword}
+    className="absolute right-48 px-4 flex items-center justify-center h-full text-zinc-600 font-bold md:text-2xl"
+  >
+    {showPassword ? "Cacher" : "Afficher"}
+  </button>
+</div>
         </div>
         <div className="input-group">
-          <label htmlFor="confirmationMotDePasse" className="text-slate-200 font-bold 
+          {/* <label htmlFor="confirmationMotDePasse" className="text-slate-200 font-bold 
           md:text-3xl">
             Confirmation du mot de passe
-          </label>
+          </label> */}
           <div className="input-container md:px-48">
             <input
-              className="bg-orange-100 rounded-md text-center
-             mobile:w-full
-            md:mb-3 md:mt-3 md:text-2xl md:w-full md:h-12"
+             className="bg-orange-100 rounded-md text-center
+             mobile:w-full 
+             md:w-full md:mb-4 md:mt-4 md:text-2xl md:h-12 md:pl-3 md:pr-16"
               type={showPasswordConfirm ? "text" : "password"}
               name="confirmationMotDePasse"
-              placeholder="confirmation de mot de passe"
+              placeholder="Confirmation de mot de passe"
               value={formData.confirmationMotDePasse}
               onChange={handleChange}
             />
             <button
               type="button"
               onClick={toggleShowPasswordConfirm}
-              className="text-slate-200 font-bold
+              className="absolute right-56 px-4 flex items-center justify-center h-full text-zinc-600 font-bold
               md:text-2xl"
             >
               {showPasswordConfirm ? "Cacher" : "Afficher"}
