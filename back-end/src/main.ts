194800e979b926509c8af5f7483d59042a9b0008
Tortosa <sofianetortosa@gmail.com>
2024-03-13 12:11:33 +0100

    maj

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -24,9 +24,9 @@ import { PrismaService } from './shared/prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Configuration des en-têtes CORS
+  
   app.enableCors({
-    origin: 'http://localhost:5173', // Ajoute ici l'origine de ton frontend
+    origin: 'http://localhost:5173', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -36,3 +36,14 @@ async function bootstrap() {
 
 bootstrap();
 
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as process from 'process';
+// import { PrismaService } from './shared/prisma/prisma.service';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT);
+//   app.get(PrismaService);
+// }
+// bootstrap();
