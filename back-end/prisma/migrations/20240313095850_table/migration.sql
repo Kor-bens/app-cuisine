099fe1fb298da2f999fcd29192ac23d0346066b9
Tortosa <sofianetortosa@gmail.com>
2024-03-13 12:07:52 +0100

    migrate

--- back-end/prisma/migrations/20240313095850_table/migration.sql
+++ back-end/prisma/migrations/20240313095850_table/migration.sql
@@ -6,6 +6,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `motDePasse` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
