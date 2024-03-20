b7cde8b09413d88aaad744f7ad360db7a5355eb7
Tortosa <sofianetortosa@gmail.com>
2024-03-20 18:48:41 +0100

    connexion user

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './user.dto';
+import { LoginUserDto } from './loginUser.dto';
 
 @Controller('users')
 export class UserController {
@@ -13,6 +14,21 @@ export class UserController {
       return { message: 'Inscription réussie!', user: newUser };
   } catch (error) {
       return { message: error.message }; // Retourner le message d'erreur
+  }
+}
+
+@Post('connexion')
+async connectUser(@Body() loginUserDto: LoginUserDto) {
+  try {
+    // Appelez une fonction dans le service user pour gérer la connexion de l'utilisateur
+    const user = await this.userService.connectUser(loginUserDto);
+    if (!user) {
+      throw new Error('Nom d\'utilisateur ou mot de passe incorrect.');
+    }
+    // Si l'utilisateur est trouvé, vous pouvez retourner un message de succès ou l'utilisateur lui-même
+    return { message: 'Connexion réussie!', user };
+  } catch (error) {
+    return { message: error.message }; // Retourner le message d'erreur
   }
 }
 }
