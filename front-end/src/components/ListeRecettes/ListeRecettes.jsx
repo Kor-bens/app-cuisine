779ac14b0d7817c74c80b82ddf2a59ba68e58c54
Tortosa <sofianetortosa@gmail.com>
2024-03-21 17:51:09 +0100

    style

--- front-end/src/components/ListeRecettes/ListeRecettes.jsx
+++ front-end/src/components/ListeRecettes/ListeRecettes.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 // TODO AFFICHER LA LISTE DES RECETTES PAR UTILISATEUR
 export default function ListeRecettes() {
   const [listeRecettes, setListeRecettes] = useState([]);
+  const [selectedRecette, setSelectedRecette] = useState(null);
 
   useEffect(() => {
     const fetchListeRecettes = async () => {
       try {
         const response = await axios.get('http://localhost:3003/recettes');
         setListeRecettes(response.data);
-        console.log(response.data)
       } catch (error) {
         console.error('Erreur lors de la récupération des données :', error);
       }
@@ -18,30 +18,46 @@ export default function ListeRecettes() {
     fetchListeRecettes();
   }, []);
 
+  const handleRecetteClick = (recette) => {
+    setSelectedRecette(recette);
+  };
+
   return (
-    <div>
+    <>
+    <div className='grid grid-cols-2 gap-1 text-center'>
+
+    </div>
+    <div className='col-span-1 cursor-pointer'>
       {listeRecettes.map((recette, index) => (
-        <div key={recette.id} className='border-solid border-2 border-sky-500 mb-2'>
-        <h3>Recette {index + 1}</h3>
-          <h2>Recette : {recette.title}</h2>
-          <p>{recette.description}</p>
-          <h3>Ingrédients :</h3>
-          <ul>
-            {recette.ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient.nom} - {ingredient.quantite}</li>
-            ))}
-          </ul>
-          <h3>Instructions :</h3>
-          <ol>
-            {recette.instructions.map((instruction, index) => (
-              <li key={index}>{instruction.etape}</li>
-            ))}
-          </ol>
-          <button className='py-1 px-1 border-solid border-1 border-black mr-2 rounded-lg bg-sky-500 hover:bg-sky-700 text-white'>Modifier</button>
-          <button className='py-1 px-1 border-solid border-1 border-black rounded-lg bg-red-500 hover:bg-red-700 text-white' >Supprimer</button>
+        <div key={recette.id} className='border-solid border-2 border-gray-500 mb-2  hover:bg-stone-800 hover:opacity-80'
+         onClick={() => handleRecetteClick(recette)}>
+          <h2 className='text-slate-50 font-bold'>Recette : {recette.title}</h2>
         </div>
       ))}
     </div>
+
+<div>
+  {selectedRecette && (
+  <div className='border-solid border-2 border-sky-500 mb-2 hover:bg-stone-800 hover:opacity-80'>
+    <h2 className='text-slate-50 font-bold'>Recette : {selectedRecette.title}</h2>
+    <p className='text-slate-50 font-bold'>{selectedRecette.description}</p>
+    <h3 className='text-slate-50 font-bold'>Ingrédients :</h3>
+    <ul className='text-slate-50 font-bold'>
+      {selectedRecette.ingredients.map((ingredient, index) => (
+        <li key={index}>{ingredient.nom} - {ingredient.quantite}</li>
+      ))}
+    </ul>
+    <h3 className='text-slate-50 font-bold'>Instructions :</h3>
+    <ol className='text-slate-50 font-bold'>
+      {selectedRecette.instructions.map((instruction, index) => (
+        <li key={index}>{instruction.etape}</li>
+      ))}
+    </ol>
+    <button className='py-1 px-1 border-solid border-1 border-black mr-2 rounded-lg bg-sky-500 hover:bg-sky-700 text-white'>Modifier</button>
+    <button className='py-1 px-1 border-solid border-1 border-black rounded-lg bg-red-500 hover:bg-red-700 text-white' >Supprimer</button>
+  </div>
+)}
+</div>
+</>
   );
 }
-
