218263a7fbb0653c0ad51dc5ba1f36f25e099db6
Tortosa <sofianetortosa@gmail.com>
2024-03-22 14:36:08 +0100

    maj style

--- front-end/src/components/MaCuisine/MaCuisine.jsx
+++ front-end/src/components/MaCuisine/MaCuisine.jsx
@@ -1,40 +1,55 @@
 import NavBar from "../Dashboard/components/Navbar";
 import ListeRecettes from "../ListeRecettes/ListeRecettes";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import RecetteFormulaire from "../AjoutRecette/RecetteFormulaire";
 
 function MaCuisine() {
   const [showRecetteFormulaire, setShowRecetteFormulaire] = useState(false);
-    const [showListeRecette, setShowListeRecette] = useState(false);
+  const [showListeRecette, setShowListeRecette] = useState(true);
+  const [showButtonAfficher, setShowButtonAfficher] = useState(false);
+  const [showButtonAjouter, setShowButtonAjouter] = useState(true);
 
-    const toggleRecetteFormulaire = () => {
-      setShowRecetteFormulaire(!showRecetteFormulaire);
-      setShowListeRecette(false)
+  const toggleRecetteFormulaire = () => {
+    setShowRecetteFormulaire(!showRecetteFormulaire);
+    setShowListeRecette(false);
+    setShowButtonAfficher(true);
+    setShowButtonAjouter(false);
   };
   
   const toggleShowListRecette = () => {
-      setShowListeRecette(!showListeRecette);
-      setShowRecetteFormulaire(false)
+    setShowListeRecette(!showListeRecette);
+    setShowRecetteFormulaire(false);
+    setShowButtonAfficher(false);
+    setShowButtonAjouter(true);
   };
-  //TODO styliser l'affichage des boutons et des composants 
+
+  // Afficher la liste de recettes par défaut
   return (
     <>
-      <NavBar />
-      <div className='grid grid-cols-5 gap-1 text-center h-full
-                      mobile:grid-col-1 grid-rows-1'>
-      <div className='col-span-0 border flex flex-col rounded-md
-                      mobile:col-span-5 mobile:row-16 mobile:border-none'>
-        <div className="flex flex-col text-slate-50 font-bold
-                       mobile:flex-row mobile:justify-around ">
-        <button onClick={toggleShowListRecette} className="mobile:border rounded-lg mobile:px-3 mobile:py-1">Afficher mes recettes</button>
-        <button onClick={toggleRecetteFormulaire} className="mobile:border rounded-lg mobile:px-3 mobile:py-1">Ajouter une recette</button>
+      <div className='grid grid-cols-1 gap-1 text-center h-full
+                      mobile:grid-col-1 mobile:grid-rows-0'>
+        <div className=' border flex flex-col rounded-md
+                       mobile:flex-row mobile:justify-end mobile:row-16 mobile:border-none'>
+          <div className="flex flex-col text-slate-50 font-bold
+                         ">
+            {showButtonAfficher && (
+              <button onClick={toggleShowListRecette} className="mobile:border rounded-lg mobile:px-3 mobile:py-1">
+                Afficher mes recettes
+              </button>
+            )}
+            {showButtonAjouter && (
+              <button onClick={toggleRecetteFormulaire} className="mobile:border rounded-lg mobile:px-3 ">
+                Ajouter une recette
+              </button>
+            )}
+          </div>
         </div>
+        {showListeRecette && <ListeRecettes />}
+        {showRecetteFormulaire && <RecetteFormulaire />}
       </div>
-      {showListeRecette && <ListeRecettes />}
-      {showRecetteFormulaire && <RecetteFormulaire />}
-     </div>
+            <NavBar />
     </>
   );
 }
+
 export default MaCuisine;
