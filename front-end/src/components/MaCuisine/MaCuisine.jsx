73722b3e43dc056dc3967839c5d51d1a96f1ada0
Tortosa <sofianetortosa@gmail.com>
2024-03-22 11:53:26 +0100

    style affichage boutons

--- front-end/src/components/MaCuisine/MaCuisine.jsx
+++ front-end/src/components/MaCuisine/MaCuisine.jsx
@@ -17,14 +17,18 @@ function MaCuisine() {
       setShowListeRecette(!showListeRecette);
       setShowRecetteFormulaire(false)
   };
+  //TODO styliser l'affichage des boutons et des composants 
   return (
     <>
       <NavBar />
-      <div className='grid grid-cols-5 gap-1 text-center h-full'>
-      <div className='col-span-0 border flex flex-col rounded-md'>
-        <div className="flex flex-col text-slate-50 font-bold">
-        <button onClick={toggleShowListRecette}>Afficher mes recettes</button>
-        <button onClick={toggleRecetteFormulaire}>Ajouter une recette</button>
+      <div className='grid grid-cols-5 gap-1 text-center h-full
+                      mobile:grid-col-1 grid-rows-1'>
+      <div className='col-span-0 border flex flex-col rounded-md
+                      mobile:col-span-5 mobile:row-16 mobile:border-none'>
+        <div className="flex flex-col text-slate-50 font-bold
+                       mobile:flex-row mobile:justify-around ">
+        <button onClick={toggleShowListRecette} className="mobile:border rounded-lg mobile:px-3 mobile:py-1">Afficher mes recettes</button>
+        <button onClick={toggleRecetteFormulaire} className="mobile:border rounded-lg mobile:px-3 mobile:py-1">Ajouter une recette</button>
         </div>
       </div>
       {showListeRecette && <ListeRecettes />}
