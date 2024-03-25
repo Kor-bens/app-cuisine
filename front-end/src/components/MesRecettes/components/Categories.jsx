097ca90a7830a1ebfcbe1cb7f26cf3447ce751ae
Tortosa <sofianetortosa@gmail.com>
2024-03-25 02:25:40 +0100

    création composant

---
+++ front-end/src/components/MesRecettes/components/Categories.jsx
@@ -0,0 +1,11 @@
+function Categories({ children }) {
+  return (
+    //TODO afficher les # catégories 
+    <>
+    { children }
+    <h1 className=" italic
+     mobile:text-2xl mobile:mt-4 mobile:ml-4" style={{ color: '#8B7979' }}>Catégories</h1>
+    </>
+  )
+}
+export default Categories
