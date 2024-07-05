402a936e67162ef7d404e4a70e755c215e0975f6
Tortosa <sofianetortosa@gmail.com>
2024-07-05 20:54:11 +0200

    maj connexion redirection et username

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accueil from "./components/Accueil/Accueil";
 import Connexion from "./components/Connexion/Connexion";
@@ -11,19 +11,22 @@ import MesRecettes from "./components/Recette/MesRecettes/MesRecettes";
 //TODO ROUTE ACCESSIBLE SEULEMENT POUR LES CONNECTER : DASHBOARD, MA CUISINE
 //TODO DECONNEXION + TIMER DECONNEXION
 //TODO BOUTON "+" POUR L'AJOUT DE RECETTE
+//TODO AFFICHAGE USERNAME DANS DASHBOARD et accueil
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [userName, setUserName] = useState("");
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/accueil" element={<Accueil userName={userName} />} />
           <Route path="/ajout" element={<AjoutRecette />} />
-          <Route path="/inscription" element={<FormulaireInscription />} />
-          <Route path="/connexion" element={<Connexion />} />
-          <Route path="/mes-recettes" element={<MesRecettes />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/mes-recettes-categories" element={<MesRecettes />} />
+          <Route path="/inscription" element={isAuthenticated ? <Dashboard /> : <FormulaireInscription />} />
+          <Route path="/connexion" element={isAuthenticated ? <Dashboard /> : <Connexion setUserName={setUserName} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/mes-recettes" element={<MesRecettes userName={userName}/>} />
+          <Route path="/dashboard" element={<Dashboard userName={userName} />} />
+          <Route path="/mes-recettes-categories" element={<MesRecettes userName={userName}/>} />
           {/* <Route path="/mes-recettes" element={<MesRecettes />} /> */}
         </Routes>
       </BrowserRouter>
