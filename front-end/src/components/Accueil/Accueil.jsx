2bf9c923176cadb8906cfb079aea088d67f42db6
Tortosa <sofianetortosa@gmail.com>
2024-07-05 20:56:40 +0200

    régler affichage username

--- front-end/src/components/Accueil/Accueil.jsx
+++ front-end/src/components/Accueil/Accueil.jsx
@@ -4,14 +4,14 @@ import './accueil.css'
 import Header from "../Header";
 import ListeRecettes from "../Recette/ListeRecettes/ListeRecettes";
 //TODO AFFICHER LES POSTS DES USERS ?? DERNIERE PUBLICATION ??
-function Accueil() {
+function Accueil({userName}) {
   return (
     <>
     <Header><p className=" italic 
     mobile:absolute mobile:top-0 mobile:left-0 mobile:ml-4 mobile:mt-12 mobile:text-4xl " style={{ color: '#8B7979' }}>CookBook</p></Header> 
     
         <NavBar />
-      <h1 className="text-gray-700" style={{ color: '#8B7979' }}>Home</h1>
+      <h1 className="text-gray-700" style={{ color: '#8B7979' }}>Home {userName}</h1>
       <ListeRecettes />
     </>
   );
--- front-end/src/components/Dashboard/Dashboard.jsx
+++ front-end/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,18 @@
-import NavBar from "./components/Navbar";
-import React, { useState } from "react";
+import NavBar from "../common/navbar/Navbar";
+import Header from "../Header";
+import { useState } from "react";
 // TODO CREATION DU DASHBOARD
-function Dashboard() {
+function Dashboard( {userName} ) {
+  console.log("Dashboard userName:", userName )
   return (
     <>
+   <div>
+   <Header><p className=" italic 
+    mobile:absolute mobile:top-0 mobile:left-0 mobile:ml-4 mobile:mt-12 mobile:text-4xl " style={{ color: '#8B7979' }}>{userName}</p></Header> 
    <NavBar />
+      <h1>Bienvenue, {userName}!</h1>
+      {/* Le reste du contenu du tableau de bord */}
+    </div>
     </>
   )
 }
