06332e91560071366ae0a92a28dca2af95b3a91e
Tortosa <sofianetortosa@gmail.com>
2024-07-08 23:07:08 +0200

    maj

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,6 +8,8 @@ import ListeRecettes from "./components/Recette/ListeRecettes/ListeRecettes";
 import FormulaireInscription from "./components/Inscription/Inscription";
 import Dashboard from "./components/Dashboard/Dashboard";
 import MesRecettes from "./components/Recette/MesRecettes/MesRecettes";
+import NavBar from "./components/common/navbar/Navbar"
+
 //TODO ROUTE ACCESSIBLE SEULEMENT POUR LES CONNECTER : DASHBOARD, MA CUISINE
 //TODO DECONNEXION + TIMER DECONNEXION
 //TODO BOUTON "+" POUR L'AJOUT DE RECETTE
@@ -18,16 +20,16 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <NavBar isAuthenticated={isAuthenticated}  setIsAuthenticated={setIsAuthenticated} />
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/accueil" element={<Accueil userName={userName} />} />
           <Route path="/ajout" element={<AjoutRecette />} />
           <Route path="/inscription" element={isAuthenticated ? <Dashboard /> : <FormulaireInscription />} />
-          <Route path="/connexion" element={isAuthenticated ? <Dashboard /> : <Connexion setUserName={setUserName} setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path="/mes-recettes" element={<MesRecettes userName={userName}/>} />
-          <Route path="/dashboard" element={<Dashboard userName={userName} />} />
+          <Route path="/connexion" element={isAuthenticated ? <Dashboard userName={userName} /> : <Connexion setUserName={setUserName} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/mes-recettes" element={isAuthenticated ? <MesRecettes userName={userName}/> : <Connexion />} />
+          <Route path="/dashboard" element={isAuthenticated ? <Dashboard userName={userName} /> : <Connexion />} />
           <Route path="/mes-recettes-categories" element={<MesRecettes userName={userName}/>} />
-          {/* <Route path="/mes-recettes" element={<MesRecettes />} /> */}
         </Routes>
       </BrowserRouter>
     </>
